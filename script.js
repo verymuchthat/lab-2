1b83d3a000082f2b000b0c3b0f0da94d187b2952
Autumn Odelia Marfurt Breakenridge <marf0006@algonquinlive.com>
2023-09-21 16:38:48 -0400

    completes lab 2!

---
+++ script.js
@@ -0,0 +1,6 @@
+const firstName = 'Autumn';
+console.log ('My Name is' + firstName);
+
+const favNumber = 222;
+const squared= favNumber * favNumber;
+ console.log (`The squared of ${favNumber} is ${squared}.`);
